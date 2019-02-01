6d7fca9840724a24731e091cf72e4c08b2b304b5
Nilam Dahal <dahalnilam90@gmail.com>
2019-01-31 18:55:16 -0800

    used pixijs

--- src/App/Pages/HomePage/HomePage.tsx
+++ src/App/Pages/HomePage/HomePage.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import * as PIXI from "pixi.js";
-import { Stage, Sprite } from "@inlet/react-pixi";
+import { PixiComponent } from "Src/App/Components/Pixi/PixiComponent";
 
 const image = require("Assets/tank.png");
 
 export default class Homepage extends React.Component {
   render() {
+    const width = 500;
+    const height = 500;
+
     return (
-      <div>
-        <Stage width={500} height={500}>
-          <Sprite image={image} x={0} y={0} />
-        </Stage>
-      </div>
+      <>
+        <PixiComponent width={width} height={height} />
+      </>
     );
   }
 }
